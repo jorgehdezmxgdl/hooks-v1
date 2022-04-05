23f14fc040a03d75140d70de75ba07b40cf516ee
jorge fausto hernandez <jorgehdezmxgdl@gmail.com>
2022-04-05 10:08:09 -0500

    Hooks v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+function App1() {
+  const estado = React.useState()
+  const valor = estado[0]
+  const setValor = estado[1]
+  console.log("valor", valor)
+  console.log("funcion", setValor);
+
+  return (<div> Hooks inicio </div>)
+}
+
+function App2() {
+  const [valor, setValor] = useState(0)
+  
+  return (
+     <div> 
+        <p> Dale click al boton para incrementar el contador</p>
+        <button onClick={() => setValor(valor + 1)} > Incrementa </button>
+        <p> El valor del contador es: {valor} </p>
+     </div>
+  )
+}
 
 function App() {
+  const [_json, setJson] = useState({
+    nombre: "Alexei",
+    edad: 0
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div> 
+        <p> Nombre:  {_json.nombre} </p>
+        <input type="text" 
+          onChange={(e) => { 
+          setJson({..._json, 
+          nombre: e.target.value}) 
+        }} />
+     </div>
+  )
 }
+
+
 
 export default App;
